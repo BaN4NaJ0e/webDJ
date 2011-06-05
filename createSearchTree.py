92cb3f07a724bf627be452abcf012413a5b11182
Chris D <theonlyhankmoody@googlemail.com>
2011-06-05 23:31:30 +0200

    cleaned up code

--- createSearchTree.py
+++ createSearchTree.py
@@ -6,25 +6,25 @@ from Cheetah.Template import Template
 import votedb
 
 class Album:
-    def __init__(self, name, coverpath, year):
-        self.name = name
-        self.coverpath = coverpath
-        self.year = year
+	def __init__(self, name, coverpath, year):
+		self.name = name
+		self.coverpath = coverpath
+		self.year = year
 
 class Track:
-    def __init__(self, name, trackid, votes, runtime):
-        self.name = name
-        self.id = trackid
-        self.votes = votes
-        self.runtime = runtime
+	def __init__(self, name, trackid, votes, runtime):
+		self.name = name
+		self.id = trackid
+		self.votes = votes
+		self.runtime = runtime
 
 class Chartitem:
-    def __init__(self, artist, song, votes, albumart, trackid):
-        self.artist = artist
-        self.song = song
-        self.votes = votes
-        self.albumart = albumart
-        self.id = trackid
+	def __init__(self, artist, song, votes, albumart, trackid):
+		self.artist = artist
+		self.song = song
+		self.votes = votes
+		self.albumart = albumart
+		self.id = trackid
 
 class SingleTrack():
 	def __init__(self, artist, title, album, albumart):
@@ -47,7 +47,7 @@ def handleVote(trackid, like, ip):
 		if like:
 			cursor.execute("""UPDATE musiclib SET votes= votes + 1 WHERE id==?;""", t )
 		else:
-		    # keine negative votezahl zulassen
+			# keine negative votezahl zulassen
 			if int(song[0][3]) > 0 :
 				cursor.execute("""UPDATE musiclib SET votes= votes - 1 WHERE id==?;""", t )
 		connection.commit()
@@ -60,67 +60,68 @@ def handleVote(trackid, like, ip):
 		return False
 
 def buildHTML():
-	# get now playing track info
-    nowPlayingTuple = mpdPlayer.nowPlaying()
-    if len(nowPlayingTuple) > 0:
-        currentTrack = SingleTrack(nowPlayingTuple[0][0],nowPlayingTuple[0][1],nowPlayingTuple[0][2],nowPlayingTuple[0][3])
-    else:
-        currentTrack = SingleTrack('Artist','Title','Album','images/no-album.png')
+	# get now playing track info from mpd
+	nowPlayingTuple = mpdPlayer.nowPlaying()
+	if len(nowPlayingTuple) > 0:
+		currentTrack = SingleTrack(nowPlayingTuple[0][0],nowPlayingTuple[0][1],nowPlayingTuple[0][2],nowPlayingTuple[0][3])
+	else:
+		currentTrack = SingleTrack('Artist','Title','Album','images/no-album.png')
 	
 	# open sqlite db connection
-    connection = sqlite3.connect("mucke.db")
-    cursor = connection.cursor()
+	connection = sqlite3.connect("mucke.db")
+	cursor = connection.cursor()
 	
+	# get current top10 voted songs
+	cursor.execute("""SELECT artist, title, votes, albumart, id FROM musiclib WHERE votes > '0' ORDER BY votes DESC LIMIT 10;""")
+	topvotesTuple = cursor.fetchall()
+	
+	chartList = []
+	for i in topvotesTuple:
+		myChartItem = Chartitem(i[0],i[1], i[2], i[3], i[4])
+		chartList.append(myChartItem)
+	
+	####################
+	# REQUEST SONG TREE
+	####################
 	# abfragen aller artists ohne doppelte eintraege
-    cursor.execute("""SELECT DISTINCT artist FROM musiclib;""")
-    artists = cursor.fetchall()
+	cursor.execute("""SELECT DISTINCT artist FROM musiclib;""")
+	artists = cursor.fetchall()
 	
-	# get current top10 songs
-    cursor.execute("""SELECT artist, title, votes, albumart, id FROM musiclib WHERE votes > '0' ORDER BY votes DESC LIMIT 10;""")
-    topvotesTuple = cursor.fetchall()
-    
-    chartList = []
-    for i in topvotesTuple:
-        myChartItem = Chartitem(i[0],i[1], i[2], i[3], i[4])
-        chartList.append(myChartItem)
-    
-    artistsList = []
+	requestSongTree = []
 	
-	# get album names for every artist
-    for artist in artists:
-        #print(artist[0].encode('latin-1'))
-        t = (artist[0],)
-        cursor.execute("""SELECT DISTINCT album FROM musiclib WHERE artist=?;""", t )
-        albenTuple = cursor.fetchall()
-        alben = []
-        for album in albenTuple:
-			#print "  ->> "+album[0]
-            t = (album[0],)
-			# get path to albumart folder.jpg
-            cursor.execute("""SELECT DISTINCT albumart FROM musiclib WHERE album=?;""", t )
-            pathTuple = cursor.fetchall()
-			# get album release year
-            cursor.execute("""SELECT DISTINCT year FROM musiclib WHERE album=?;""", t )
-            yearTuple = cursor.fetchall()
-            myAlbum = Album(album[0].encode('latin-1'), str(pathTuple[0][0]), str(yearTuple[0][0]))
-			#alben.append(album[0].encode('latin-1'))
-            alben.append(myAlbum)
-            
-            # get all tracknames for this album
-            cursor.execute("""SELECT DISTINCT title,id,votes,tracklength FROM musiclib WHERE album=?;""", t )
-            trackTuple = cursor.fetchall()
-            tracks = []
+	# get all album names for every single artist
+	for artist in artists:
+		t = (artist[0],)
+		# frage von jedem künstler alle alben ab
+		cursor.execute("""SELECT DISTINCT album FROM musiclib WHERE artist=?;""", t )
+		albenTuple = cursor.fetchall()
+		alben = []
+		for album in albenTuple:
+			t = (album[0],)
+			# get path to albumart folder.jpg and release year
+			cursor.execute("""SELECT DISTINCT albumart, year FROM musiclib WHERE album=?;""", t )
+			resultTupel = cursor.fetchall()
+			myAlbum = Album(album[0].encode('latin-1'), str(resultTupel[0][0]), str(resultTupel[0][1]))
+			alben.append(myAlbum)
+			
+			# get all tracks for each album
+			cursor.execute("""SELECT DISTINCT title,id,votes,tracklength FROM musiclib WHERE album=?;""", t )
+			trackTuple = cursor.fetchall()
+			tracks = []
 			#pprint.pprint( trackTuple )
-            for track in trackTuple:
-                myTrack = Track( track[0], track[1] , track[2], track[3])
-                tracks.append(myTrack)
-            
-            artistsList.append([artist[0],alben,tracks])
-	#pprint.pprint(artistsList)
-    
-    print "## updated index.html"
-    # close db connection
-    connection.close()
-    nameSpace = {'charts': chartList, 'artists': artistsList, 'current': currentTrack }
-    t= Template(file="templates/index.html", searchList=[nameSpace])
-    return t
+			for track in trackTuple:
+				myTrack = Track( track[0], track[1] , track[2], track[3])
+				tracks.append(myTrack)
+			
+		# add all informations to tree
+		requestSongTree.append([artist[0],alben,tracks])
+	pprint.pprint(requestSongTree)
+	
+	# close db connection
+	connection.close()
+	
+	nameSpace = {'charts': chartList, 'artists': requestSongTree, 'current': currentTrack }
+	t= Template(file="templates/index.html", searchList=[nameSpace])
+	
+	print "## updated index.html"
+	return t
