e36260a656a4a047863700f067c76a5826964d56
Chris D <theonlyhankmoody@googlemail.com>
2011-06-07 14:34:56 +0200

    new and faster request song pages

--- createSearchTree.py
+++ createSearchTree.py
@@ -43,9 +43,9 @@ class Historyitem:
 		self.albumart = albumart
 		self.lastplayed = lastplayed
 
-
+# put incoming song vote from user into db
 def handleVote(trackid, like, ip):
-	# check if user has enough votes left
+	# check if user has already voted for this song
 	if votedb.checkQuota(trackid, ip):
 		# open sqlite db connection
 		connection = sqlite3.connect("mucke.db")
@@ -68,6 +68,77 @@ def handleVote(trackid, like, ip):
 		return True
 	else: 
 		return False
+
+# get html with all available artists
+def buildArtists():
+	# open sqlite db connection
+	connection = sqlite3.connect("mucke.db")
+	cursor = connection.cursor()
+	
+	# abfragen aller artists ohne doppelte eintraege
+	cursor.execute("""SELECT DISTINCT artist FROM musiclib;""")
+	artistsTupel = cursor.fetchall()
+	
+	artists = []
+	
+	for artist in artistsTupel:
+		artists.append(artist[0])
+	
+	# close db connection
+	connection.close()
+	
+	nameSpace = {'artists': artists}
+	t= Template(file="templates/artists.html", searchList=[nameSpace])
+	
+	print "## updated artists.html"
+	return t
+
+# get all albums and tracks for the given artist
+def buildAlben(artistname):
+	# open sqlite db connection
+	connection = sqlite3.connect("mucke.db")
+	cursor = connection.cursor()
+	
+	t = (artistname,)
+	# frage vom künstler alle alben ab
+	cursor.execute("""SELECT DISTINCT album FROM musiclib WHERE artist=?;""", t )
+	albenTuple = cursor.fetchall()
+	alben = []
+	
+	# von jedem album die tracks abfragen
+	for album in albenTuple:
+		t = (album[0],)
+		# get path to albumart folder.jpg and release year
+		cursor.execute("""SELECT DISTINCT albumart, year FROM musiclib WHERE album=?;""", t )
+		resultTupel = cursor.fetchall()
+		
+		# get all tracks for each album
+		alb = (album[0],artistname)
+		cursor.execute("""SELECT DISTINCT title,id,votes,tracklength FROM musiclib WHERE album=? AND artist=?;""", alb )
+		albumtracksTuple = cursor.fetchall()
+		tracks = []
+		#pprint.pprint( albumtracksTuple )
+		for track in albumtracksTuple:
+			#				title , 	id , 		votes, 	tracklength
+			myTrack = Track( track[0], track[1] , track[2], track[3])
+			tracks.append(myTrack)
+		
+		#						name					coverpath				year				all tracks
+		myAlbum = Album(album[0].encode('latin-1'), str(resultTupel[0][0]), str(resultTupel[0][1]), tracks )
+		alben.append(myAlbum)
+		
+	# close db connection
+	connection.close()
+	
+	nameSpace = {'alben': alben, 'artist': artistname}
+	t= Template(file="templates/alben.html", searchList=[nameSpace])
+	
+	print "## created songtree for: "+ artistname
+	return t
+
+##############################
+# alte methode die gesamten baum abfragt! laaangsaaam
+##############################
 
 # build request song html
 def buildRequest():
@@ -129,6 +200,9 @@ def buildHistory():
 	connection = sqlite3.connect("mucke.db")
 	cursor = connection.cursor()
 	
+	##############################
+	# get last 10 played songs
+	##############################
 	cursor.execute("""SELECT artist, title, album, albumart, lastplayed FROM musiclib WHERE lastplayed > '0' ORDER BY lastplayed DESC LIMIT 10;""")
 	historyTuple = cursor.fetchall()
 	
@@ -155,9 +229,11 @@ def buildHistory():
 	print "## updated history.html"
 	return t
 
-
-def buildHTML():
+# build main page
+def buildIndex():
+	##############################
 	# get now playing track info from mpd
+	##############################
 	nowPlayingTuple = mpdPlayer.nowPlaying()
 	if len(nowPlayingTuple) > 0:
 		currentTrack = SingleTrack(nowPlayingTuple[0][0],nowPlayingTuple[0][1],nowPlayingTuple[0][2],nowPlayingTuple[0][3])
@@ -168,9 +244,9 @@ def buildHTML():
 	connection = sqlite3.connect("mucke.db")
 	cursor = connection.cursor()
 	
-	####################
+	##############################
 	# get current top10 voted songs
-	####################
+	##############################
 	cursor.execute("""SELECT artist, title, votes, albumart, id FROM musiclib WHERE votes > '0' ORDER BY votes DESC LIMIT 10;""")
 	topvotesTuple = cursor.fetchall()
 	
--- webpy.py
+++ webpy.py
@@ -7,90 +7,105 @@ import thread
 import votedb
 from web.contrib.template import render_cheetah
 
-render = render_cheetah('templates/')        
+render = render_cheetah('templates/')
 
 urls = (
-    '/', 'index', 
-    '/images/(.*)', 'images', 
-    '/liked/(.*)', 'liked', 
-    '/hated/(.*)', 'hated',
-    '/notification/', 'notification',
-    '/request/', 'request',
-    '/history/', 'history'
+	'/', 'index', 
+	'/images/(.*)', 'images', 
+	'/liked/(.*)', 'liked', 
+	'/hated/(.*)', 'hated',
+	'/notification/', 'notification',
+	'/request/', 'request',
+	'/artists/', 'artists',
+	'/history/', 'history'
 )
 
 app = web.application(urls, globals())
 
 # html page for votes
 class liked:
-    def POST(self, id):
-    	userip = web.ctx.ip	
-        # user hat fuer einen song gestimmt
-        success = createSearchTree.handleVote(id, True, userip )	
-        if success:
-            raise web.seeother('/')
-        else:
-	        raise web.seeother('/notification/')
+	def POST(self, id):
+		userip = web.ctx.ip	
+		# user hat fuer einen song gestimmt
+		success = createSearchTree.handleVote(id, True, userip )	
+		if success:
+			raise web.seeother('/')
+		else:
+			raise web.seeother('/notification/')
 				
 class hated:
-    def POST(self, id):
-        userip = web.ctx.ip
-        # user hat gegen einen song gestimmt
-        success = createSearchTree.handleVote(id, False, userip )
-        if success:
-            raise web.seeother('/')
-        else:
-	        raise web.seeother('/notification/')
+	def POST(self, id):
+		userip = web.ctx.ip
+		# user hat gegen einen song gestimmt
+		success = createSearchTree.handleVote(id, False, userip )
+		if success:
+			raise web.seeother('/')
+		else:
+			#user hat schon für den song gestimmt
+			raise web.seeother('/notification/')
 
+# user notification (you already voted for this song)
 class notification:
-    def GET(self):
-        return render.notification()
+	def GET(self):
+		return render.notification()
 
 # history page
-class history:        
-    def GET(self):
-        historyHtml = createSearchTree.buildHistory()
-        return historyHtml
+class history:		
+	def GET(self):
+		historyHtml = createSearchTree.buildHistory()
+		return historyHtml
 
 # index page
-class index:        
-    def GET(self):
-        indexHtml = createSearchTree.buildHTML()
-        return indexHtml
+class index:		
+	def GET(self):
+		indexHtml = createSearchTree.buildIndex()
+		return indexHtml
+
+# all artists page
+class artists:		
+	def GET(self):
+		urlInput = web.input(artist = '')
+		artist = urlInput.artist
+		if artist == "":
+			requestHtml = createSearchTree.buildArtists()
+			return requestHtml
+		else:
+			requestHtml = createSearchTree.buildAlben(artist)
+			return requestHtml
 
 # request song page
-class request:        
-    def GET(self):
-        requestHtml = createSearchTree.buildRequest()
-        return requestHtml
+class request:		
+	def GET(self):
+		requestHtml = createSearchTree.buildRequest()
+		return requestHtml
 
 # handling images in website (albumart,..)
 class images:
-    def GET(self,name):
-        ext = name.split(".")[-1] # Gather extension
+	def GET(self,name):
+		ext = name.split(".")[-1] # Gather extension
 
-        cType = {
-            "png":"images/png",
-            "jpg":"image/jpeg",
-            "gif":"image/gif",
-            "ico":"image/x-icon"            }
+		cType = {
+			"png":"images/png",
+			"jpg":"image/jpeg",
+			"gif":"image/gif",
+			"ico":"image/x-icon"	}
 
-        if name in os.listdir('images'):  # Security
-            web.header("Content-Type", cType[ext]) # Set the Header
-            web.header("Cache-Control", "public")
-            web.header("Expires", "Sun, 17 Jan 2038 19:14:07 GMT")
-            return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
-        else:
-            raise web.notfound()
+		if name in os.listdir('images'):  # Security
+			web.header("Content-Type", cType[ext]) # Set the Header
+			web.header("Cache-Control", "public")
+			web.header("Expires", "Sun, 17 Jan 2038 19:14:07 GMT")
+			return open('images/%s'%name,"rb").read() # Notice 'rb' for reading images
+		else:
+			raise web.notfound()
 
 if __name__ == "__main__":
-    # create votes per user count db
-    votedb.setupDB()
-    #make independant thread for musicplayer    
-    thread.start_new_thread(mpdPlayer.manager, ()) 
-    app.run()
+	# create votes per user count db
+	votedb.setupDB()
+	#make independant thread for musicplayer	
+	thread.start_new_thread(mpdPlayer.manager, ()) 
+	app.run()
 
-    	
+		
 
 
 
